aabf4025b8a75b685b70fde10837a845bf70d088
Jesus Flores <me@jesusfloressanjose.com>
2022-06-29 16:59:46 +0200

    Print next 2 holidays, hardcoded

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 using namespace boost;
 
 int main() {
+	int publicHolidaysToCount = 2;
 	gregorian::date d{2022, 12, 24};
 	std::cout << d.day_of_week() << '\n';
+
+	gregorian::day_iterator it{d};
+	for (int i = 0; i < publicHolidaysToCount; ++i) {
+		*++it;
+		if (it->day() == 25 || it->day() == 26) {
+			std::cout << it->day_of_week() << '\n';
+		}
+	}
 }
